3865d2b17eabf541aa61986cd4a50baa7c5c10af
Mahmoud <mahmoudahmed.razek@gmail.com>
2020-09-05 06:53:58 +0200

    write code splash

--- app/src/main/java/com/example/moviesapp/SplashScreen.kt
+++ app/src/main/java/com/example/moviesapp/SplashScreen.kt
@@ -1,12 +1,43 @@
 package com.example.moviesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        textView.startAnimation(animationFadeIn)
+        textView2.startAnimation(animationFadeIn)
+
+        val intent = Intent(this, MainActivity::class.java)
+        Handler().postDelayed({
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
+        hideSystemUI()
+
+
+    }
+
+    private fun hideSystemUI() {
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
